6610dcdec0c171d8b36d7cbe2dca6c1f664627ab
Isabelle_Moura <mourabisabelle@gmail.com>
2023-07-26 10:56:57 -0300

    chore: make sorting and pagination page

--- mentors/js/index.js
+++ mentors/js/index.js
@@ -1,53 +1,128 @@
-//R = Read
-const url = "https://api-projeto-modulo-1.onrender.com/mentors";
+const url = "https://api-projeto-modulo-1.onrender.com/mentors"; // API URL
+let ascendingOrder = true; // Variable to control sorting
+let mentors = []; // Declaration of the mentors variable in the global scope
 
+// Constants for pagination
+const mentorsPerPage = 7;
+let currentPage = 1;
 
-const showMentors = (mentors) => {
+//////////////////////////////////////////
+// Function to display the list of mentors in the table dynamically
+const showMentors = (mentorsData) => {
   const tableContent = document.getElementById("tableContent");
   let mentorHtml = "";
-  
-  mentors.forEach((mentor) => {
+
+  mentorsData.forEach((mentor) => {
     mentorHtml += `
     <tr>
-    <td>${mentor.name}</td>
-    <td>${mentor.email}</td>
-        <td>
-          <button class="edit-button" id="editButton" onclick="edit(${mentor.id})"><i class="fa-solid fa-pencil" style="color: #004ce8;"></i></button>
-          <button class="delete-button" id="deleteButton" onclick="deleteButton(${mentor.id})"><i class="fa-solid fa-trash" style="color: #ff3333;"></i></button>
-        </td>
-      </tr>
-      `;
-    });
+      <td>${mentor.name}</td>
+      <td>${mentor.email}</td>
+      <td>
+        <button class="edit-button" id="editButton" onclick="edit(${mentor.id})"><i class="fa-solid fa-pencil" style="color: #004ce8;"></i></button>
+        <button class="delete-button" id="deleteButton" onclick="deleteButton(${mentor.id})"><i class="fa-solid fa-trash" style="color: #ff3333;"></i></button>
+      </td>
+    </tr>
+    `;
+  });
 
   tableContent.innerHTML = mentorHtml;
-  
-  // Ordenar a lista de mentores por ordem alfabética do nome
-  const sortByNameButton = document.getElementById("sortByNameButton");
-    sortByNameButton.addEventListener("click", () => {
-      mentors.sort((a, b) => a.name.localeCompare(b.name));
-      showMentors(mentors);
-    });
 };
 
+//////////////////////////////////////////
+// Function to sort the list of mentors by name
+
+const sortMentorsByName = (mentorsData, order) => {
+  const sortedMentors = [...mentorsData];
+
+  sortedMentors.sort((a, b) => {
+    if (order === "asc") {
+      return a.name.localeCompare(b.name); // Ascending order (A-Z)
+    } else {
+      return b.name.localeCompare(a.name); // Descending order (Z-A)
+    }
+  });
+
+  return sortedMentors;
+};
+
+// Select element for sorting
+const orderSelect = document.getElementById("orderSelect");
+
+// Event listener to detect changes in sorting
+orderSelect.addEventListener("change", () => {
+  currentPage = 1; // Reset the current page to 1 when the sorting order changes
+  displayMentors(mentors, currentPage, mentorsPerPage);
+});
+
+// Function to paginate mentors based on the current page and limit
+const paginateMentors = (mentorsData, page, perPage) => {
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  return mentorsData.slice(startIndex, endIndex);
+};
+
+// Function to display mentors for the given page and sorting order
+const displayMentors = (mentorsData, page, perPage) => {
+  const sortedMentors = sortMentorsByName(mentorsData, orderSelect.value);
+  const paginatedMentors = paginateMentors(sortedMentors, page, perPage);
+  showMentors(paginatedMentors);
+};
+
+// Function to handle pagination button click
+const handlePaginationClick = (pageNum) => {
+  currentPage = pageNum;
+  displayMentors(mentors, currentPage, mentorsPerPage);
+};
+
+// Function to create pagination buttons
+const createPaginationButtons = (totalPages) => {
+  const paginationContainer = document.getElementById("paginationContainer");
+  paginationContainer.innerHTML = ""; // Clear previous pagination buttons
+
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.classList.add("pagination-button");
+
+    // Highlight the active page button
+    if (i === currentPage) {
+      button.classList.add("active");
+    }
+
+    button.addEventListener("click", () => {
+      handlePaginationClick(i);
+    });
+
+    paginationContainer.appendChild(button);
+  }
+};
+
+// Function to calculate total number of pages
+const getTotalPages = (mentorsData, perPage) => {
+  return Math.ceil(mentorsData.length / perPage);
+};
+
+//////////////////////////////////////////
+// Function to get the list of mentors from the API
 const getMentors = async () => {
   try {
     const apiResponse = await fetch(url);
-    const mentors = await apiResponse.json();
-    
-    console.log(mentors);
-    showMentors(mentors);
+    mentors = await apiResponse.json(); // Assign API data to the mentors variable
+    const totalPages = getTotalPages(mentors, mentorsPerPage);
+    createPaginationButtons(totalPages);
+    displayMentors(mentors, currentPage, mentorsPerPage);
   } catch (error) {
     console.error(error);
   }
 };
 
-getMentors();
-//////////////////////////////////////////
+getMentors(); // Call the function to get mentors on page load
 
-//Imput de busca (Search Bar)
+//////////////////////////////////////////
+// Search Input (Search Bar)
 const searchInput = document.getElementById("searchInput");
 
-// Função para filtrar mentores por nome
+// Function to filter mentors by name
 const filterMentors = (mentors, searchTerm) => {
   searchTerm = searchTerm.toLowerCase();
   return mentors.filter((mentor) =>
@@ -55,7 +130,7 @@ const filterMentors = (mentors, searchTerm) => {
   );
 };
 
-// Função para realizar a busca e exibição dos mentores filtrados
+// Function to perform search and display filtered mentors
 const searchMentors = async (searchTerm) => {
   try {
     const apiResponse = await fetch(`${url}`);
@@ -68,14 +143,15 @@ const searchMentors = async (searchTerm) => {
 };
 
 searchInput.addEventListener("keyup", (event) => {
+  // Event listener to detect if the "Enter" key was pressed in the search
   if (event.key === "Enter") {
     const searchTerm = searchInput.value;
     searchMentors(searchTerm);
   }
 });
-//////////////////////////////////////////
 
-//Redirecionamento das páginas
+//////////////////////////////////////////
+// Page Redirection
 
 //"Side Menu"
 const mentorsPage = document.getElementById("mentorsPage");
@@ -83,6 +159,7 @@ const mentorshipPage = document.getElementById("mentorshipPage");
 const classesPage = document.getElementById("classesPage");
 const studentsPage = document.getElementById("studentsPage");
 
+// Event listeners to redirect when clicking on menu pages
 mentorsPage.addEventListener("click", function () {
   window.location.href = "../../mentors/html/index.html";
 });
@@ -99,49 +176,81 @@ studentsPage.addEventListener("click", function () {
   window.location.href = "../../students/html/index.html";
 });
 
-//Botão "Novo Mentor"
+// "New Mentor" Button
 const newButton = document.getElementById("newButton");
 
 newButton.addEventListener("click", function () {
+  // Event listener to redirect when clicking on the "New Mentor" button
   window.location.href = "../../mentors/html/register.html";
 });
 
-//Botão "Editar"
+// "Edit" Button
 const edit = (id) => {
   window.location.href = `../../mentors/html/edit.html?id=${id}`;
 };
-//////////////////////////////////////////
 
-//D = Delete
-const deleteButton = async (mentorId) => {
+// Function to delete a mentor
+const deleteMentor = async (mentorId) => {
   try {
     const response = await fetch(`${url}/${mentorId}`, {
       method: "DELETE",
     });
     console.log(response);
     if (response.ok) {
-      console.log("Mentor excluído com sucesso");
+      console.log("Mentor deleted successfully");
+      // Refresh the mentors list after successful deletion
+      await getMentors();
     } else {
-      console.error("Erro ao excluir o mentor!");
+      console.error("Error deleting the mentor!");
     }
   } catch (error) {
-    console.error("Ocorreu um erro ao excluir o mentor!", error);
+    console.error("An error occurred while deleting the mentor!", error);
   }
 };
 
-//Pegar os dados do usuário no localStorage
+// Function to delete a mentor
+const deleteButton = async (mentorId) => {
+  // Open the confirmation modal when the delete button is clicked
+  const deleteConfirmationModal = new bootstrap.Modal(
+    document.getElementById("deleteConfirmationModal")
+  );
+  deleteConfirmationModal.show();
 
-// No JavaScript da página de mentores
+  // Store the mentorId in a data attribute of the delete button in the modal
+  document
+    .getElementById("confirmDeleteButton")
+    .setAttribute("data-mentor-id", mentorId);
+};
+
+// Event listener for the "Delete" button in the confirmation modal
+document
+  .getElementById("confirmDeleteButton")
+  .addEventListener("click", async () => {
+    const mentorId = document
+      .getElementById("confirmDeleteButton")
+      .getAttribute("data-mentor-id");
+    await deleteMentor(mentorId); // Call the actual function to delete the mentor
+    // Close the confirmation modal after deleting the mentor
+    const deleteConfirmationModal = new bootstrap.Modal(
+      document.getElementById("deleteConfirmationModal")
+    );
+    deleteConfirmationModal.hide();
+  });
+
+//////////////////////////////////////////
+// Get user data from localStorage
+
 document.addEventListener("DOMContentLoaded", () => {
-  // Recupere a lista de usuários cadastrados do localStorage
-  const usersList = JSON.parse(localStorage.getItem("usersList"));
+  // In the JavaScript of the mentors page
+
+  const usersList = JSON.parse(localStorage.getItem("usersList")); // Retrieve the list of registered users from localStorage
 
   if (usersList && usersList.length > 0) {
-    // Se a lista de usuários existir e não estiver vazia, exiba o último usuário cadastrado
+    // If the list of users exists and is not empty, display the last registered user
     const lastUser = usersList[usersList.length - 1];
     document.getElementById("user-name").textContent = `${lastUser.name}`;
     document.getElementById("user-email").textContent = `${lastUser.email}`;
   } else {
-    alert("Usuário não existe!")
+    alert("User does not exist!");
   }
 });
